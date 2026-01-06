0ab12242729df13419e359f3eb79a6357a030c0c
leaderksv8 <leaderksv8@gmail.com>
2026-01-06 21:51:15 +0200

    Оновив код

--- docs/js/render.js
+++ docs/js/render.js
@@ -1,11 +1,25 @@
 export function renderActivities(data, lang) {
     const container = document.getElementById('activities-container');
     if (!container || !data) return;
+
     container.innerHTML = data.map(item => `
         <div class="b2-item-card">
-            <div class="b2-icon-box"><i class="fa-solid fa-${item.icon}"></i></div>
-            <h4>${item.title[lang]}</h4>
-            <p>${item.desc[lang]}</p>
+            <div class="b2-card-inner">
+                <!-- Лицьова сторона -->
+                <div class="b2-card-front">
+                    <div class="b2-icon-box">
+                        <i class="fa-solid fa-${item.icon}"></i>
+                    </div>
+                    <h4>${item.title[lang]}</h4>
+                    <div class="b2-flip-hint">
+                        <i class="fa-solid fa-rotate"></i>
+                    </div>
+                </div>
+                <!-- Зворотна сторона -->
+                <div class="b2-card-back">
+                    <p>${item.desc[lang]}</p>
+                </div>
+            </div>
         </div>
     `).join('');
 }
